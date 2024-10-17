0f366c79e3c31156ca447413166fb86ef22940ee
Jonas Nascimento <jonashnascimento@gmail.com>
2024-10-17 14:41:13 -0300

    create routes module

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const path = require("path")
+const routes = require("./routes")
 
-const basePath = path.join(__dirname, "templates")
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
 
-app.get('/', (req, res) => {
-    res.sendFile(`${basePath}/index.html`)
-})
+app.use(express.json())
+
+app.use('/', routes)
 
 app.listen(port, () => {
     console.log("Started server at port:", port)
